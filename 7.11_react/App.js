21cc2a8cc9df18c83a64ecfa9633b4fd17d1930e
Oleksandra Lymar <96631944+OleksandraLymar@users.noreply.github.com>
2022-07-12 10:30:40 +0300

    Create App.js

---
+++ 7.11_react/App.js
@@ -0,0 +1,15 @@
+import NinyFooter from "./components/Footer/NinyFooter";
+import NinyHeader from "./components/Header/NinyHeader";
+import FormSignUp from "./components/Forms/Auth/FormSignUp";
+import PhoneBook from "./components/PhoneBook/PhoneBook";
+
+
+function App() {
+  return (
+    <div className="App">
+        <PhoneBook></PhoneBook>
+    </div>
+  );
+}
+
+export default App;
